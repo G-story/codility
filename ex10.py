de99a17c49bf036e967ebb837d374c027c818d65
김석근 <sgkim@zuminternet.com>
2015-11-26 21:21:46 +0900

    ex10 완료

--- ex10.py
+++ ex10.py
@@ -2,14 +2,16 @@ def solution(A):
     ret = 0
     min_avg = float(A[0] + A[1]) / 2
     for i in xrange(len(A) - 1):
-        sum = A[i]
-        for j in xrange(i + 1, len(A)):
-            sum += A[j]
-            avg = float(sum) / (j - i + 1)
-            if avg < min_avg:
-                min_avg = avg
-                ret = i
+        avg = float(sum(A[i:i + 2])) / 2
+        if avg < min_avg:
+            min_avg = avg
+            ret = i
+        if i < len(A) - 2:
+            avg = float(sum(A[i:i + 3])) / 3
+        if avg < min_avg:
+            min_avg = avg
+            ret = i
     return ret
 
 
-print(solution([4, 2, 2, 5, 1, 5, 8]))
+print(solution([5, 5, 1, 1, 1]))
